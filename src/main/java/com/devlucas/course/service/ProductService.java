0559349a4db2f546529827253f3e9faea4c011ac
Lucas williams <lucaswilliamscesar@gmail.com>
2023-02-08 09:01:05 -0300

    Product

---
+++ src/main/java/com/devlucas/course/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.devlucas.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devlucas.course.entities.Product;
+import com.devlucas.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
